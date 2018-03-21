062dbab48a0a0b423c9636fbeef51e9f43205477
moonsekh <33743392+moonsekh@users.noreply.github.com>
2018-03-21 17:14:54 +0530

    Update en.js

--- language/en.js
+++ language/en.js
@@ -83,15 +83,8 @@ var Translations = {
     "PromoSettings": {
         "promo1": {
             "trackingName": "promo1",
-            "url": "http://example.com/",
+            "url": "https://www.cimbpreferred.com/",
             "label": "Promo (Top)",
-            "desktopImage": "./images/promo_portrait.png",
-            "mobileImage": "./images/promo_landscape.png"
-        },
-        "promo2": {
-            "trackingName": "promo2",
-            "url": "http://example.com/",
-            "label": "Promo (Bottom)",
             "desktopImage": "./images/promo_portrait.png",
             "mobileImage": "./images/promo_landscape.png"
         }
